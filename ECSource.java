4c5bfb3405ffe4847779f96653fb3e3f0bfde47e
Jiang <zhou.jiang@fiserv.com>
2023-11-06 14:57:22 -0500

    add solution

--- ECSource.java
+++ ECSource.java
@@ -1,17 +1,35 @@
-class Node<T> {
-  T val;
-  Node<T> next;
+//class Node<T> {
+//  T val;
+//  Node<T> next;
+//
+//  public Node(T val) {
+//    this.val = val;
+//    this.next = null;
+//  }
+//}
 
-  public Node(T val) {
-    this.val = val;
-    this.next = null;
-  }
-}
+import java.util.HashMap;
 
 class ECSource {
   public static <T> boolean isUnivalueList(Node<T> head) {
-    // todo
-    return true;
+//    HashMap<T, Integer> map = new HashMap<>();
+//    while(head!=null) {
+//        if(map.containsKey(head.val)) {
+//            return false;
+//        } else {
+//            map.put(head.val, 0);
+//        }
+//        head = head.next;
+//    }
+//    return true;
+
+      if(head.next == null) {
+          return true;
+      }
+      if(head.val == head.next.val) {
+          return false;
+      }
+      return isUnivalueList(head.next);
   }
 
   public static void main(String[] args) {
@@ -20,5 +38,20 @@ class ECSource {
 
         // Printing solution
         System.out.println((ECSource.isUnivalueList(z)));
+
+      Node<Integer> u = new Node<>(2);
+      Node<Integer> v = new Node<>(2);
+      Node<Integer> w = new Node<>(2);
+      Node<Integer> x = new Node<>(2);
+      Node<Integer> y = new Node<>(2);
+
+      u.next = v;
+      v.next = w;
+      w.next = x;
+      x.next = y;
+
+// 2 -> 2 -> 2 -> 2 -> 2
+
+      System.out.println(ECSource.isUnivalueList(u)); // true
     }
 }
--- Source.java
+++ Source.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Node<T> {
     T val;
     Node<T> next;
@@ -10,7 +12,17 @@ class Node<T> {
   
   class Source {
     public static <T> Node<T> reverseList(Node<T> head) {
-      // todo
+        Node prev = null; // in order to reverse a linked list, we need a previous node, current node, and next node
+        Node curr = head;
+        Node next = null;
+        while(curr != null) { // starting with the curr node, which is the head node
+            next = curr.next;  // increment next
+            curr.next = prev; //  next is set to previous in order to keep prev as the initial value
+            prev = curr; // previous is set to current
+            curr = next; // current = next
+        }
+        // as we go through the nodes, we are storing the previous node,
+        head = prev;
       return head;
     }
     
@@ -25,9 +37,30 @@ class Node<T> {
         // Printing solution
         Node<String> head = Source.reverseList(x);
 
+        Node<String> a = new Node<>("a"); // next = b, curr.next = null, prev = a, curr = b;
+        Node<String> b = new Node<>("b"); // next = c, curr.next = a, prev b, curr = c;
+        Node<String> c = new Node<>("c"); // next = d, curr.next = b, prev = c, curr = d;
+        Node<String> d = new Node<>("d");
+        Node<String> e = new Node<>("e");
+        Node<String> f = new Node<>("f");
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+// a -> b -> c -> d -> e -> f
+
+        Node<String> head2 = Source.reverseList(a);// f -> e -> d -> c -> b -> a
+
         while (head != null) {
-            System.out.println(head.val);
+            System.out.print(head.val);
             head = head.next;            
+        }
+        System.out.println();
+        while (head2 != null) {
+            System.out.print(head2.val);
+            head2 = head2.next;
         }
     }
   }
